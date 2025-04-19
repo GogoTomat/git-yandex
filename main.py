26e6fa03ee7280378fb1d1979c469750c0f4a9d1
mkomi <mkomissarov207@gmail.com>
2025-04-19 15:07:10 +0500

    [+] latte

--- main.py
+++ main.py
@@ -3,106 +3,22 @@ import io
 import sqlite3
 
 from PyQt6.QtWidgets import QWidget, QApplication, QTableWidgetItem
-from PyQt6 import uic
-from PyQt6.QtCore import Qt, QPoint
+from PyQt6.QtCore import Qt
 from addCoffee import addCoffeeValue
+from ui.main_ui import Ui_Form
 
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>630</width>
-    <height>299</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="pushButton">
-   <property name="geometry">
-    <rect>
-     <x>450</x>
-     <y>260</y>
-     <width>171</width>
-     <height>32</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>Получить данные</string>
-   </property>
-  </widget>
-  <widget class="QTableWidget" name="tableWidget">
-   <property name="geometry">
-    <rect>
-     <x>10</x>
-     <y>10</y>
-     <width>611</width>
-     <height>251</height>
-    </rect>
-   </property>
-   <property name="columnCount">
-    <number>7</number>
-   </property>
-   <attribute name="horizontalHeaderCascadingSectionResizes">
-    <bool>false</bool>
-   </attribute>
-   <attribute name="horizontalHeaderDefaultSectionSize">
-    <number>87</number>
-   </attribute>
-   <attribute name="horizontalHeaderHighlightSections">
-    <bool>true</bool>
-   </attribute>
-   <attribute name="horizontalHeaderShowSortIndicator" stdset="0">
-    <bool>false</bool>
-   </attribute>
-   <attribute name="horizontalHeaderStretchLastSection">
-    <bool>false</bool>
-   </attribute>
-   <column/>
-   <column/>
-   <column/>
-   <column/>
-   <column/>
-   <column/>
-   <column/>
-  </widget>
-  <widget class="QPushButton" name="addButton">
-   <property name="geometry">
-    <rect>
-     <x>260</x>
-     <y>260</y>
-     <width>171</width>
-     <height>32</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>Добавить значение</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
-
-
-class Coffee(QWidget):
+class Coffee(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
         self.pushButton.clicked.connect(self.set_values)
         self.addButton.clicked.connect(self.add_values)
         self.tableWidget.itemChanged.connect(self.change_value)
-        self.con = sqlite3.connect("coffee.sqlite")
+        self.con = sqlite3.connect("data//coffee.sqlite")
         self.cur = self.con.cursor()
         self.modified = dict()
 
